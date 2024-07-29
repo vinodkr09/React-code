42a9202ff8f390f41431635a1d5c17f5bcbcad05
vinodkr09 <vinod.krmahi@gmail.com>
2024-07-29 11:56:06 +0530

    currency convertor ptoject did not run

---
+++ 07_currencyconvertor/src/hooks/UseCurrencyInfo.js
@@ -0,0 +1,18 @@
+// design of custom hooks
+
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json`)
+        .then((response) => response.json())
+        .then((response) => setData(response[currency]))
+        console.log(data)
+    }, [currency])
+    console.log(data)
+    return data
+
+}
+
+export default useCurrencyInfo;
