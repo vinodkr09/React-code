2d89c4cd934cad60e636043580d3d3d7586dc44f
vinodkr09 <vinod.krmahi@gmail.com>
2024-08-21 01:57:50 +0530

    blog platform added

---
+++ 14_blogplatform_using_appwrite/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL)
+  
+
+  return (
+    <>
+      <p> A blog with Appwrite </p>
+    </>
+  )
+}
+
+export default App
